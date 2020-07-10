083c14aca12edb8fb5e37d9fbb6fa00e43425772
dyp <duanyapeng1@163.com>
2020-07-10 14:52:45 +0800

    'a'

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
+import markdown
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post
  
 def index(request):
@@ -7,4 +9,11 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.body = markdown.markdown(post.body,
+                                  extensions=[
+                                      'markdown.extensions.extra',
+                                    #   'markdown.extensions.codehilite',
+                                      'markdown.extensions.fenced_code',
+                                      'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
