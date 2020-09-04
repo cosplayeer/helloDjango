31f8f2a1f37978cddc26b3f538d5e5e8012ac1ec
dyp <duanyapeng1@163.com>
2020-09-04 17:29:19 +0800

    add production haystack

--- blogproject/settings/production.py
+++ blogproject/settings/production.py
@@ -6,4 +6,5 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.zmrenwu.com']
 # ALLOWED_HOSTS = ['hellodjango-blog-tutorial-demo.zmrenwu.com']
-# HAYSTACK_CONNECTIONS['default']['URL'] = 'http://hellodjango_blog_tutorial_elasticsearch:9200/'
+# 搜索设置
+HAYSTACK_CONNECTIONS['default']['URL'] = 'http://elasticsearch_local:9200/'
